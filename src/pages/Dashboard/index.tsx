207897abb70dcb6055dc0304346127bd1beca461
linhduong2209 <linh.dh22900@gmail.com>
2024-03-09 18:36:13 +0700

    update layout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { GridLayout } from "../../components/layout";
+import { GridLayout } from "../../components/Layout";
 import Hero from "../../containers/hero";
 
 export const Dashboard: React.FC = () => {
@@ -20,5 +20,5 @@ export const Dashboard: React.FC = () => {
 
 const ContentWrapper = styled.div.attrs({
   className:
-    "col-span-full xl:col-start-2 xl:col-end-12 space-y-10 xl:space-y-[72px] mb-10 xl:mb-20 pb-10"
+    "col-span-full xl:col-start-2 xl:col-end-12 space-y-10 xl:space-y-[72px] mb-10 xl:mb-20 pb-10",
 })``;
